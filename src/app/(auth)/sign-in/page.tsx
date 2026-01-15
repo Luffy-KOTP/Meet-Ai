26c765c5a098b7ec60eb08562a8dfcf7e8c180dd
Luffy-KOTP <luffy-email@example.com>
2026-01-15 17:19:39 +0100

    authentication ui complete

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInView } from "@/modules/auth/ui/views/Sign-in-view"
+
+const Page = () => {
+  return <SignInView />
+}
+
+export default Page
+
+// http://localhost:3000/auth/sign-in
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from "@/modules/auth/ui/views/Sign-up-view";
+
+const Page = () => {
+  return <SignUpView />
+}
+
+export default Page
+
+// http://localhost:3000/auth/sign-up
